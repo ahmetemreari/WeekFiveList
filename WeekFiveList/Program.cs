473bdfad1f3f362c20cea2af4ffd5f3fafdbe28c
AHMET EMRE ARI <ahmetemreari67@gmail.com>
2024-11-04 16:46:30 +0300

    first commit

---
+++ WeekFiveList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //liste oluştur
+        List<string> ziyaretciListesi = new List<string>();
+
+        // Ziyaretçi ekle
+        ziyaretciListesi.Add("Behlül");
+        ziyaretciListesi.Add("Bihter");
+        ziyaretciListesi.Add("Nihal");
+        ziyaretciListesi.Add("Adnan");
+        ziyaretciListesi.Add("Firdevs");
+
+        // isimleri ekrana yazdıralım
+        Console.WriteLine("Patika Plus gala gecesine davetli ziyaretçiler:");
+        foreach (string ziyaretci in ziyaretciListesi)
+        {
+            Console.WriteLine(ziyaretci);
+        }
+    }
+}
